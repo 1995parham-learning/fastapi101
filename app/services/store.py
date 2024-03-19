91b7ec5f14d69112a27445b60638c08181e02a68
Parham Alvani <parham.alvani@gmail.com>
2024-03-19 19:55:00 +0000

    feat: replace users list with a dictionary

--- app/services/store.py
+++ app/services/store.py
@@ -5,18 +5,39 @@ Storages for users.
 from app import domain
 
 
+class DuplicateEntry(Exception):
+    """
+    Duplicate user identification.
+    """
+
+    def __init__(self, user_id: str):
+        self.user_id = user_id
+
+        super().__init__()
+
+    def __str__(self):
+        return f"duplicate user ({self.user_id})"
+
+
 class Storage:
     """
     In memory storage for users.
     """
 
-    storage: list[domain.User] = []
+    storage: dict[str, domain.User] = {}
 
     def __init__(self):
         pass
 
     def append(self, user: domain.User):
-        self.storage.append(user)
+        """
+        Register new user.
+        """
+        if user.id not in self.storage:
+            self.storage[user.id] = user
 
     def all(self) -> list[domain.User]:
-        return self.storage
+        """
+        Return all the registered users.
+        """
+        return list(self.storage.values())
