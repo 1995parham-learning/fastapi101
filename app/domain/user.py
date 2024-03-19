cb29206b80e1627ccb81debacba82b107c6425fc
Parham Alvani <parham.alvani@gmail.com>
2024-03-19 19:42:07 +0000

    feat: update user model

--- app/domain/user.py
+++ app/domain/user.py
@@ -1,7 +1,14 @@
+import datetime
+
 import pydantic
 
 
 class User(pydantic.BaseModel):
+    """
+    The user model to handle user in the application.
+    """
+
+    id: str
     first_name: str
     last_name: str
-    age: int
+    registration_date: datetime.date
